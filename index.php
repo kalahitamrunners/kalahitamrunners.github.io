07e67a6e1b96cd5bd44155acd36f5c02a34323c2
kalahitamrunners <149651983+kalahitamrunners@users.noreply.github.com>
2023-11-07 10:31:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="google-adsense-account" content="ca-pub-6716925749747603">
     <title>KALA HITAM RUNNER</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
-    <meta name="google-adsense-account" content="ca-pub-6716925749747603">
 
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
